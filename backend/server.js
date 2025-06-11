cd34b908c3481a8693f16b7f44ea55f3527760cb
SriMukheshSuru <21ee01025@iitbbs.ac.in>
2025-06-11 16:25:08 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/products",productRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-    
+
     app.get(/(.*)/,(req,res) =>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
     });
@@ -33,4 +33,3 @@ app.listen(PORT,() =>{
 
 
 
-// password : WYEdRvyPBR77WRbp
